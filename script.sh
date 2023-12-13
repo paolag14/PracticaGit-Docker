91ada212cd9d7976afbe74ded3502a11b924d84e
Paola Gómez <90631352+paolag14@users.noreply.github.com>
2023-12-13 23:15:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,5 @@ if [ $(sudo docker container list -a | grep httpd | wc -l ) -gt 0 ]; then
 httpd | grep -Eo '^[^ ]+')
 else
     sudo docker build -t my-web-page .
-    sudo docker run -d -p 8081:80 -v
-"$(pwd)/public-html/:/usr/local/apache2/htdocs" my-web-page
+    sudo docker run -d -p 8081:80 -v "$(pwd)/public-html/:/usr/local/apache2/htdocs" my-web-page
 fi
